c89008c75e0f6b2ffd6d1f1adb969815310fd305
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-06-25 22:45:15 -0700

    I coded Data layer

---
+++ Shopping.Core/Repositories/ICustomUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Shopping.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Core.Repositories
+{
+    public interface ICustomUserRepository
+    {
+        Task<bool> Validate(string email, string password);
+
+        Task<CustomUser> FindById(int id);
+
+        Task<CustomUser> FindByEmail(string email);
+    }
+}
---
+++ Shopping.Data/Repositories/CustomUserRespository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Core.Models;
+using Shopping.Core.Repositories;
+
+namespace Shopping.Data.Repositories
+{
+    internal class CustomUserRespository:ICustomUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CustomUserRespository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CustomUser> FindByEmail(string email)
+        {
+            return await _context.customUsers.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<CustomUser> FindById(int id)
+        {
+            return await _context.customUsers.FindAsync(id);
+        }
+
+        public async Task<bool> Validate(string email, string password)
+        {
+            return await _context.customUsers.AnyAsync(x => x.Email == email && x.Password == password);
+        }
+    }
+}
