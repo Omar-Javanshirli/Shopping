eb7cbb2bba85db38682a896699aaad90817a502f
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-06-25 23:11:50 -0700

    new update

--- Shopping.Data/UnitOfWork.cs
+++ Shopping.Data/UnitOfWork.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Core.UnityOfWork;
 
 namespace Shopping.Data
 {
-    internal class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommmitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
     }
 }
